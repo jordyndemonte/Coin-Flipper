3cfe8fecea5a2e457c77fa6951c20834c75dac22
Jordyn <demontejordyn8@gmail.com>
2022-10-07 00:02:26 -0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,53 @@
 // TODO: Declare any global variables we need
-
+let headsRolls = 0
+let tailsRolls = 0
 
 document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('flip').addEventListener('click', () => {
+        let flippedHeads = Math.random() < 0.5
+        if (flippedHeads) {
+            document.getElementById('penny-image').src = 'assets/images/penny-heads.jpeg'
+            document.getElementById('message').textContent = 'You Flipped Heads!'
+            headsRolls += 1
+        }
+        else{ 
+            document.getElementById('penny-image').src = 'assets/images/penny-tails.jpeg'
+            document.getElementById('message').textContent = 'You Flipped Tails!'
+            tailsRolls += 1
+        }
+        let total = headsRolls +  tailsRolls 
+
+        let percentHeads = 0
+        let percentTails = 0
+        if (total > 0) {
+            percentHeads = Math.round((headsRolls / total) * 100)
+            percentTails = Math.round((tailsRolls / total) * 100)
+        }
+        document.getElementById('heads').textContent = headsRolls
+        document.getElementById('heads-percent').textContent = percentHeads + '%'
+        document.getElementById('tails').textContent = tailsRolls
+        document.getElementById('tails-percent').textContent = percentTails + '%'
+    })
+    document.getElementById('clear').addEventListener('click', function (){
+        headsRolls = 0
+        tailsRolls = 0
+        document.getElementsById('message').textContent = 'Let\'s Get Rolling!'
+        let total = headsRolls + tailsRolls
+        let percentHeads = 0
+        let percentsTails = 0
+        if (total > 0) {
+            percentHeads = Math.round((headsRolls / total) * 100)
+            percent = Math.round((tailsRolls / total) * 100)
+        }
+        document.getElementById('heads').textContent = headsRolls
+        document.getElementById('heads-percent').textContent = percentHeads + '%'
+        document.getElementById('tails').textContent = tailsRolls
+        document.getElementById('tails-percent').textContent = percentTails + '%'
+    })
+
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+  
 
     // TODO: Add event listener and handler for flip and clear buttons
 
